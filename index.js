b0172a82e97906182b22faa979922ec5ebb471a6
cutiegirl526 <kayladiniega@gmail.com>
2016-11-02 08:08:19 -1000

    Notes for Java

---
+++ index.js
@@ -0,0 +1,23 @@
+// This is a comment
+/* this is a block comment */
+
+// this is a string
+var myName = "Jon";
+
+// this is a integer
+var myAge = 12;
+
+// boolean type
+var = booleanExample = true;
+
+// unassaigned variable
+undefined
+
+// not a number
+NaN
+
+//array
+
+//objects
+
+var camelCase = "yay"
