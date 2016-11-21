e5f52177a2af2709061f80e3db0f7f6dd56a05c1
cutiegirl526 <kayladiniega@gmail.com>
2016-11-21 08:12:28 -1000

    New notes but catch up

---
+++ arrays_objects.js
@@ -0,0 +1,8 @@
+var tan = ["john", "jon", "cool", 3, 11, 7, 9, null, undefined, ["no shirt", 27, null, ]]
+console.log(tan[9][0]);
+console.log(tan[1]);
+
+var yoloSwag = {
+	isaiah : "beast"
+	hair : "sunshine"
+}
